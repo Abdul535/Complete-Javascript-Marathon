1393b9356ba6a6244c31158f4c65cd43eb123a93
Abdul535 <syedabdullah535@gmail.com>
2022-07-19 17:53:10 +0530

    day2

---
+++ Phase-1/test.js
@@ -0,0 +1,82 @@
+console.log("hello world");
+
+// var ab = 'abc'
+// var ab
+// console.log(ab);
+
+// let abc='qasim';
+// let abc
+// console.log(abc);
+
+// {
+    // let a=5; //local 
+    // a=9;
+    // var a=5; //global variable
+    // const a=5;
+// }
+// console.log(a)
+
+// const ab=5;
+// ab=6;
+// console.log(ab)
+
+// q   a   s   i   m
+// 0   1   2   3   4
+// let name = "Faizan";
+// console.log(name[0]);
+// console.log(name[1]);
+// console.log(name[2]);
+// console.log(name[3]);
+// console.log(name[4]);
+// console.log("length is ", name.length)
+//trim,toUpperCase,toLowerCase,slice
+
+//single args slice(start index (inclusive) )
+//double args slice(start index, end index (exclusive)  )
+// f   a   i   z   a   n
+// 0   1   2   3   4   5
+// let res = name.slice(3)
+// let res = name.slice(1,4);
+// console.log(res);
+
+// let res = name.trim() //remove white spaces
+// console.log(name);
+// console.log(res);
+
+// let res = name.toUpperCase();
+// console.log(res);
+
+// let res = name.toLowerCase();
+// console.log(res);
+
+// let marks = 50;
+// let str = "my marks is " + marks;
+// let str = `my marks is ${marks}`;
+// console.log(str);
+
+//null
+//undefined
+//BigInt
+//typeof 
+
+// var CFI = null;
+// console.log(typeof CFI, CFI);
+
+// var CFI; 
+// console.log(typeof CFI, CFI);
+
+// BigInt
+// console.log(Number.MAX_SAFE_INTEGER);
+
+// var bignumber =  BigInt(9847238489238492);
+// var bignumber = 892189481491n; 
+// var number = 52094857329085732908570392857309857235830925n;
+// console.log(number);
+// console.log(typeof bignumber, bignumber);
+
+console.log(typeof true, true);
+console.log(typeof false, false);
+//type coersion
+
+
+
