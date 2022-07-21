d85484646878421930d007d6f23b1952437efe87
Abdul535 <syedabdullah535@gmail.com>
2022-07-21 19:52:38 +0530

    day4

--- Phase-1/7.js
+++ Phase-1/7.js
@@ -5,9 +5,26 @@
 // toLowerCase() -return the string with lower case
 // slice() -slice the string and return the value
     // slice(start index,end index(exclusive)) ;
-    // slice(start index(exclusive));
+    //slice(start index(inclusive));
+// concat()
+// length
+// indexOf()
+// valueOf()
+// toString()
+// replace()
+// repeat()
+// slice()
+// toLowerCase()
+// toUpperCase()
+// split()
+// substr()
+// trim()
+
 let str = "hello";
 
+// s   l   i   c   e
+// 0   1   2   3   4 -array can be accessed
+// -5  -4  -3  -2  -1 -.slice() can also access negative indexing
 let res = str.slice(1,6);
 // let res = str.slice(1);
 console.log(res)
